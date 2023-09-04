029d76020209e77bcdcb67baa5b8532f5ee888e6
Brn <bruno.trabalhos@outlook.com>
2023-09-04 20:11:56 -0300

    representação das funções básicas de uma SmartTv

---
+++ trilha-dio/exercicio-tv-smart/src/SmartTv.java
@@ -0,0 +1,39 @@
+public class SmartTv{
+    Boolean ligada = false;
+    Integer canal = 1;
+    Integer volume = 25;
+
+    public SmartTv(){
+    }
+
+    public void ligarTv(){
+        this.ligada = true;
+    }
+
+    public void desligarTv(){
+        this.ligada = false;
+    }
+
+    public void aumentarVolume(){
+        System.out.println("+1 volume");
+        this.volume++;
+    }
+
+    public void diminuirVolume(){
+        System.out.println("-1 volume");
+        this.volume--;
+    }
+
+    public void aumentarCanal(){
+        this.canal++;
+    }
+
+    public void diminuirCanal(){
+        this.canal--;
+    }
+
+    public void mudarCanal(int canal){
+        this.canal = canal;
+    }
+
+}
--- trilha-dio/exercicio-tv-smart/src/Usuario.java
+++ trilha-dio/exercicio-tv-smart/src/Usuario.java
@@ -1,5 +1,28 @@
-public class App {
+public class Usuario {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Bem vindo a SmartTv");
+        SmartTv smartTv = new SmartTv();
+        System.out.println("A TV está ligada? " + smartTv.ligada);
+        System.out.println("Canal: " + smartTv.canal);
+        System.out.println("Volume: " + smartTv.volume);
+        smartTv.ligarTv();
+        smartTv.aumentarVolume();
+        smartTv.aumentarVolume();
+        smartTv.aumentarVolume();
+        smartTv.aumentarVolume();
+        smartTv.aumentarVolume();
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.aumentarCanal();
+        smartTv.aumentarCanal();
+        smartTv.diminuirCanal();
+        System.out.println("A TV está ligada? " + smartTv.ligada);
+        System.out.println("Canal: " + smartTv.canal);
+        System.out.println("Volume: " + smartTv.volume);
+        smartTv.mudarCanal(15);
+        System.out.println("Canal: " + smartTv.canal);
+        smartTv.desligarTv();
+        System.out.println("A TV está ligada? " + smartTv.ligada);  
+
     }
 }
