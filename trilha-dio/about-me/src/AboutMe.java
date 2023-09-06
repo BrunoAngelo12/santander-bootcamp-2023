61c9c078874efb6f642578cc180c6c7681589b43
Brn <bruno.trabalhos@outlook.com>
2023-09-05 21:36:24 -0300

    exercicio de argumentos

---
+++ trilha-dio/about-me/src/AboutMe.java
@@ -0,0 +1,10 @@
+public class AboutMe {
+    public static void main(String[] args) throws Exception {
+        String nome = args[0];
+        String sobrenome = args[1];
+        String idade = args[2];
+        String altura = args[3];
+
+        System.out.println("Olá, me chamo " + nome + " " + sobrenome +  " tenho " + idade + " anos e " + altura + "de altura");
+    }
+}
