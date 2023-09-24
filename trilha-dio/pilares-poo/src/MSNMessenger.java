15198fbb4a062d6220b0e4b449d2e4f462f20d8d
Brn <bruno.trabalhos@outlook.com>
2023-09-23 22:05:07 -0300

    aplicado o conceito de encapsulamento

--- trilha-dio/pilares-poo/src/MSNMessenger.java
+++ trilha-dio/pilares-poo/src/MSNMessenger.java
@@ -1,5 +1,21 @@
 public class MSNMessenger {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    
+    public void enviarMensagem(){
+        validarConectadoInternet();        
+        System.out.println("Enviando mensagem.."); 
+        salvarHistoricoInternet();       
     }
+
+    public void receberMensagem(){
+        System.out.println("Recebendo mensagem..");        
+    }
+
+    private void validarConectadoInternet(){
+        System.out.println("Validando se está conectado à internet..");        
+    }
+
+    private void salvarHistoricoInternet(){
+        System.out.println("Salvando o histórico da conversa..");        
+    }
+
 }
---
+++ trilha-dio/pilares-poo/src/MeuComputador.java
@@ -0,0 +1,7 @@
+public class MeuComputador {
+    public static void main(String[] args) throws Exception {
+        MSNMessenger msn = new MSNMessenger();
+        msn.enviarMensagem();
+        msn.receberMensagem();
+    }
+}
