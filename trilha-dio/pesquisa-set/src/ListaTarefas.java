4a156695898842b8ab66078eac9919ddb954bb58
Brn <bruno.trabalhos@outlook.com>
2023-10-06 23:10:07 -0300

    criado alguns metodos solicitados no exercicio

--- trilha-dio/pesquisa-set/src/ListaTarefas.java
+++ trilha-dio/pesquisa-set/src/ListaTarefas.java
@@ -8,4 +8,68 @@ public class ListaTarefas {
         this.setTarefas = new HashSet<>();
     }
 
+    public void adicionarTarefa(String descricao, Boolean tarefaCompleta){
+        setTarefas.add(new Tarefa(descricao, tarefaCompleta));
+    }
+
+    @Override
+    public String toString() {
+        return "ListaTarefas [setTarefas=" + setTarefas + "]";
+    }
+
+    public void removerTarefa(String descricao){
+        for(Tarefa t : setTarefas){
+            Tarefa tarefaParaRemover = null;
+            if(t.getDescricao().equalsIgnoreCase(descricao)){
+                tarefaParaRemover = t;
+                setTarefas.remove(t);
+                break;
+            }
+        }
+    }
+
+    public void exibirTarefas(){
+        System.out.println(setTarefas);
+    }
+
+    public int contarTarefas(){
+        return setTarefas.size();
+    }
+
+    public Set<Tarefa> obterTarefasConcluidas(){
+        Set<Tarefa> tarefasConcluidas = new HashSet<>();
+        for(Tarefa t : setTarefas){
+            if(t.getTarefaCompleta().equals(true)){
+                tarefasConcluidas.add(t);
+            }
+        }
+        return tarefasConcluidas;
+    }
+
+    public Set<Tarefa> obterTarefasPendentes(){
+        Set<Tarefa> tarefasPendentes = new HashSet<>();
+        for(Tarefa t : setTarefas){
+            if(t.getTarefaCompleta().equals(false)){
+                tarefasPendentes.add(t);
+            }
+        }
+        return tarefasPendentes;
+    }
+
+    public void marcarTarefaConcluida(String descricao){
+        for(Tarefa t : setTarefas){
+            if(t.getDescricao().equalsIgnoreCase(descricao)){
+                t.setTarefaCompleta(true);
+            }
+        }
+    }
+
+    public void marcarTarefaPendente(String descricao){
+        for (Tarefa t : setTarefas){
+            if(t.getDescricao().equalsIgnoreCase(descricao)){
+                t.setTarefaCompleta(false);
+            }
+        }
+    }
+
 }
--- trilha-dio/pesquisa-set/src/Tarefa.java
+++ trilha-dio/pesquisa-set/src/Tarefa.java
@@ -2,10 +2,18 @@ public class Tarefa {
     private String descricao;
     private Boolean tarefaCompleta;
 
-    public Tarefa(String descricao){
+    public Tarefa(String descricao, Boolean tarefaCompleta){
         this.descricao = descricao;
-        this.tarefaCompleta = false;
+        this.tarefaCompleta = tarefaCompleta;
     }
+
+    
+
+    public void setTarefaCompleta(Boolean tarefaCompleta) {
+        this.tarefaCompleta = tarefaCompleta;
+    }
+
+
 
     public String getDescricao() {
         return descricao;
@@ -17,7 +25,51 @@ public class Tarefa {
 
     @Override
     public String toString() {
-        return "{" + descricao + ", " + tarefaCompleta + "}";
+        return "{" + this.descricao + ", " + this.tarefaCompleta + "}";
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((descricao == null) ? 0 : descricao.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Tarefa other = (Tarefa) obj;
+        if (descricao == null) {
+            if (other.descricao != null)
+                return false;
+        } else if (!descricao.equals(other.descricao))
+            return false;
+        return true;
+    }
+
+    public static void main(String[] args) {
+        ListaTarefas sTarefas = new ListaTarefas();
+        sTarefas.adicionarTarefa("teste", false);
+        sTarefas.adicionarTarefa("teste 2", true);
+        /*System.out.println(sTarefas.toString());
+        sTarefas.removerTarefa("teste");
+        System.out.println(sTarefas.toString());
+        sTarefas.adicionarTarefa("teste");
+        System.out.println(sTarefas.toString());
+        sTarefas.exibirTarefas();
+        System.out.println(sTarefas.contarTarefas());
+        System.out.println(sTarefas.obterTarefasConcluidas());
+        System.out.println(sTarefas.obterTarefasPendentes());*/
+        sTarefas.marcarTarefaConcluida("teste");
+        sTarefas.exibirTarefas();
+        sTarefas.marcarTarefaPendente("teste");
+        sTarefas.exibirTarefas();
     }
 
 }
