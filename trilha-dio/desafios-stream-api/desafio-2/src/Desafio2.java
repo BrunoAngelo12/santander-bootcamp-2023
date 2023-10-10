d0c52fbfa272535a20469e0628d2138d874bc3e8
Brn <bruno.trabalhos@outlook.com>
2023-10-09 21:44:46 -0300

    resolucao do desafio 2 stream Api

---
+++ trilha-dio/desafios-stream-api/desafio-2/src/Desafio2.java
@@ -0,0 +1,14 @@
+import java.util.stream.Stream;
+
+public class Desafio2 {
+        
+    public static void main(String[] args) {
+            /*Desafio 2 - Imprima a soma dos números pares da lista:Utilizando a Stream API, realize a soma 
+            dos números pares da lista e exiba o resultado no console.*/
+
+            Lista lista = new Lista();
+            System.out.println("A soma dos números pares é = " + lista.somaPares());
+
+        }
+}
+
---
+++ trilha-dio/desafios-stream-api/desafio-2/src/Lista.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+public class Lista {
+    List <Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
+
+    public Lista() {
+    }
+
+    @Override
+    public String toString() {
+        return "Lista [numeros=" + numeros + "]";
+    }
+
+    public int somaPares(){
+        int soma = numeros.stream().mapToInt(Integer::intValue).filter(n -> n % 2 == 0).sum();
+        return soma;
+    }
+
+}
